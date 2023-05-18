41f72833a717b7814868c1f15dcf5ce6bb42f583
belkisarslan <belkisarslan33@gmail.com>
2023-05-18 12:34:58 +0300

    meanmode

--- coderbyte2.js
+++ coderbyte2.js
@@ -20,3 +20,45 @@ const divisionStringified = (n1, n2) => {
     return arr.reverse().join("")
 }
 console.log(divisionStringified(123456789, 1000))
+
+//meanMode
+const meanMode = (arr) => {
+
+    const mean = calcMean(arr)
+    const mode = calcMode(arr)
+    //eğer ortalama değer mod değere eşitse 1 değilse 0 dön
+    return mean === mode ? 1 : 0
+}
+const calcMean = (arr) => {
+
+    let sum = 0
+    for(let i=0; i<arr.length; i++){
+        sum += arr[i]
+    }
+    return sum / arr.length
+}
+const calcMode = (arr) => {
+    let freq = {}
+    for(let j = 0; j < arr.length; j++){
+        if(arr[j] in freq){
+            freq[arr[j]] = freq[arr[j]] + 1
+        }else{
+            freq[arr[j]] = 1
+        }
+    }
+
+    const max = Math.max(...Object.values(freq))
+    // return freq (bu aşamada her sayıını kaçar adet olduğunu döndürdük)
+    // return max  (bu aşamada en fazla tekrar eden sayının kaç tane olduğuna ulaştık)
+    return iterateOverObj(freq, max)
+}
+
+const iterateOverObj = (freq, max) => {
+  for(let j in freq){
+    if(freq[j] === max){
+        return parseInt(j)
+    }
+  }
+}
+
+console.log(meanMode([5, 3, 3, 3, 3, 3, 1]))
