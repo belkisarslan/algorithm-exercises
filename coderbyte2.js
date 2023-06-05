4abed15b3424f8ffd3d044aadff21182178ca03d
belkisarslan <belkisarslan33@gmail.com>
2023-06-05 20:23:49 +0300

    arrayMatching

--- coderbyte2.js
+++ coderbyte2.js
@@ -315,3 +315,42 @@ const waveSorting = (arr) => {
  return newArr
 }
 console.log(waveSorting([3, 6, 5, 10, 20, 7]))
+
+//arrayMatching
+const arrayMatching = (arr) => {
+    //rakamları array olarak döndüren regex
+    //! önemli olan nokta şu ki: 99'u [9, 9] şeklinde ayırmasın [99] olarak alsın
+    const arr1 = arr[0].match(/[0-9]+/gi)
+    const arr2 = arr[1].match(/[0-9]+/gi)
+
+    let longArr = "equel"
+    if(arr1.length > arr2.length) longArr = "first"
+    if(arr2.length > arr1.length) longArr = "second"
+
+    let newArr = []
+    if(longArr === "equel") newArr = createArray(arr1, arr2, arr2.length)
+    if(longArr === "first") newArr = createArray(arr1, arr2, arr2.length, "first")
+    if(longArr === "second") newArr = createArray(arr1, arr2, arr1.length, "second")
+
+    let result = newArr[0]
+    for(let i= 1; i<newArr.length; i++){
+        result = result + "-" + String(newArr[i])
+    }
+    return result
+    
+}
+
+const createArray = (a, b, len, balance="equel") => {
+
+    let arr = []
+    for(let i = 0; i< len; i++){
+        //rakamlar string formatında olduğu için parseInt kullandık
+        arr.push(parseInt(a[i]) + parseInt(b[i]))
+    }
+    if(balance === "equel") return arr
+    if(balance === "first") return arr.concat(a.slice(-(a.length - b.length)))
+    if(balance === "second") return arr.concat(b.slice(-(b.length - a.length)))
+
+    
+}
+console.log(arrayMatching(["[1, 2, 5, 6]","[5, 2, 8, 11]"]))
